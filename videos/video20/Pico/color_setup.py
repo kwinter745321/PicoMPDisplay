9adfc8a4c6958e5c720272e462dccf09edf0d11d
kwinter745321 <160160815+kwinter745321@users.noreply.github.com>
2025-02-13 14:02:02 -0500

    video20 files
    Uploaded files

--- videos/video20/Pico/color_setup.py
+++
@@ -1,8 +0,0 @@
-# color_setup.py
-#
-# Modified 28 January 2025
-# By 2025 KW Services to support SSD1963 800X480 LCD Display.
-# MIT License
-# MicroPython v1.20.0 on 2023-04-26; Raspberry Pi Pico with RP2040
-#
-from drivers.ili9341_pico import ssd
